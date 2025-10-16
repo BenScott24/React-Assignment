ddfc6b9bc7f70b73765235e1f9a81b5d5c45c205
BenScott24 <scobr003@mymail.unisa.edu.au>
2025-10-16 16:55:51 +1030

    create Editor component that exposes methods to parent using ref, allows parent to run, stop, get and set code directly, supports updating runtime options like volume, bpm and instrument along with setting up internal editor reference for Strudel integration

--- src/Components/Editor.js
+++ src/Components/Editor.js
@@ -1,4 +1,26 @@
-import { forwardRef, useEffect, useImperativeHandle, useRef } from "react";
+import { forwardRef, use, useEffect, useImperativeHandle, useRef } from "react";
 import { initStrudel, StrudelMirror, getAudioContext, webaudioOutput, transpiler, evalScope, registerSynthSounds, initAudioOnFirstClick, } from "@strudel/web";
 import { registerSoundfonts } from "@strudel/soundfonts";
 
+const Editor = forwardRef(({ code }, ref) => {
+    const editorDiv = useRef(null);
+    const strudelEditor = useRef(null);
+
+    useImperativeHandle(ref, () => ({
+        evaluate: () => strudelEditor.current?.evaluate(),
+        stop: () => strudelEditor.current?.stop(),
+        setCode: (newCode) => strudelEditor.current?.setCode(newCode),
+        getCode: () => strudelEditor.current?.getCode(),
+        
+        setRuntimeOptions: (opts) => {
+            if (strudelEditor.current?.repl?.state) {
+                strudelEditor.current.repl.state.runtimeOptions = { ...(strudelEditor.current.repl.runtimeOptions || {}), ...opts };
+            }
+        },
+
+        repl: strudelEditor.current?.repl,
+
+    }));
+
+    
+}
