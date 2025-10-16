69ab39f0893d43b4824a13eda090e78dca81352f
BenScott24 <scobr003@mymail.unisa.edu.au>
2025-10-16 14:10:37 +1030

    setting up state variables and editor reference in App component

--- src/App.js
+++ src/App.js
@@ -7,3 +7,14 @@ import D3Waveform from './Components/D3Waveform';
 import useProcessor from './Components/useProcessor';
 import { stranger_tune } from './Tunes/stranger_tune';
 
+export default function App() {
+  const [code, setCode] = userState(stranger_tune);
+  const [p1State, setP1State] = userState('ON');
+  const [volume, setVolume] = userState(0.8);
+  const [bpm, setBpm] = userState(120);
+  const [reverb, setReverb] = userState(false);
+  const [instrument, setInstrument] = userState('piano');
+  const [alert, setAlert] = userState(null);
+  const editorRef = useRef(null);
+
+  
