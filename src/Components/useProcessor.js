2293d1ddcbaa9f60c4afc3be3fa4740b282828bc
BenScott24 <scobr003@mymail.unisa.edu.au>
2025-10-18 13:03:49 +1030

    adding in the method to process text and add playback with error handling and alert message

--- src/Components/useProcessor.js
+++ src/Components/useProcessor.js
@@ -10,3 +10,16 @@
             setAlert?.({ type: 'danger', text: 'Processing and playback failed.'});
         }
     };
+
+    const processAndPlay = (text) => {
+        try {
+            const processed = processText(text);
+            if(editorRef.current) {
+                editorRef.current.setCode(processed);
+                setTimeout(() => { editorRef.current.evaluate(); }, 150);
+            }
+        } catch (error) {
+            console.error(error);
+            setAlert?.({ type: 'danger', text: 'Processing and playback failed.'});
+        }
+    };
