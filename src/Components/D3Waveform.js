329b935593292b9acbd8da8ca6c9ebe830e72a71
BenScott24 <scobr003@mymail.unisa.edu.au>
2025-10-18 13:34:19 +1030

    rendering in wavefrom in D3wavefrom by mapping code line lengths ton visual data

---
+++ src/Components/D3Waveform.js
@@ -0,0 +1,17 @@
+import { useEffect, useRef } from "react";
+import * as d3 from "d3";
+
+export default function D3Waveform({ code }) {
+    const svgRef = useRef(null);
+
+    useEffect(() => {
+        const svg = d3.select(svgRef.current);
+        const width = 4000;
+        const height = 120;
+        svg.attr('viewBox', `0 0 ${width} ${height}`).style('width', '100%').style('height', '120px');
+
+        svg.selectAll("*").remove();
+
+        const lines = (code || '').split('\n');
+        const data = lines.length ? lines.map((line, index) => ({ x: index, y: line.length })) : [{x: 0, y: 0}];
+
