94cca0d0915b5b19e24b956e7294c496c0aa8c79
BenScott24 <scobr003@mymail.unisa.edu.au>
2025-10-18 12:27:46 +1030

    adding preprocessing options with ON/OFF radio buttons

---
+++ src/Components/Options.js
@@ -0,0 +1,16 @@
+export default function Options({ p1State, setP1State}) {
+    return (
+        <div>
+            <h6>Preprocessing Options</h6>
+            <div className="form-check">
+                <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" checked={p1State === 'ON'} onChange={() => setP1State('ON')} />
+                <label className="form-check-label" htmlFor="flexRadioDefault1">On</label>
+            </div>
+            <div className="form-check">
+                <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked={p1State === 'OFF'} onChange={() => setP1State('OFF')} />
+                <label className="form-check-label" htmlFor="flexRadioDefault2">Off</label>
+            </div>
+        </div>
+
+        
+    )
