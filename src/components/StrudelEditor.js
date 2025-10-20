c39b2cb801a60aee59f10ea1c8f4024624276e16
BenScott24 <scobr003@mymail.unisa.edu.au>
2025-10-20 16:23:44 +1030

    wrapping the editor, canvas and controls in the main tag so its easier to edit the styles in the css file

--- src/components/StrudelEditor.js
+++ src/components/StrudelEditor.js
@@ -56,17 +56,10 @@ export default function StrudelEditor() {
 }, []);
 
     return (
-        <main>
-            <div className="row">
-                <div className="col-md-8" style={{maxHeight: '50vh', overflowY: 'auto'}}>
-                    <div id="editor"/>
-                    <div id="output"/>
-                    </div>
-                    <div className="col-md-4">
-                        <Controls/>
-                </div>
-            </div>
-            <CanvasRoll/>
+        <main className="editor-container">
+            <div id="editor" />
+            <CanvasRoll />
+            <Controls />
         </main>
     );
 }
