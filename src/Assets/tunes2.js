57696d5aa9f6732fc24ce3c1a15782dcba19ed4e
BenScott24 <scobr003@mymail.unisa.edu.au>
2025-10-20 16:49:08 +1030

    making the melody

---
+++ src/Assets/tunes2.js
@@ -0,0 +1,7 @@
+export const birds_of_a_feather = `setcps(105/60/4)
+
+let m1 =
+note("<[D@3 A@2 ~ D@2] [Cs@2 ~ A@2 ~ Cs@2]>".add("12,24")).s("gm_kalimba:3").legato(1.5).fast(2)
+.attack(.025).release(.2).lp(1000)
+.room(".6:2").postgain(1.5).color('#4dbcf4')._pitchwheel({edo:12,hapRadius:3,thickness:3,circle:1})
+`
