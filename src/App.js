00b1a63f36b1a58491c85176f7d264a9b1b527b5
BenScott24 <scobr003@mymail.unisa.edu.au>
2025-10-20 17:59:29 +1030

    removing the background on the icon so its the same as the webpage done in camera settings

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import StrudelEditor from './components/StrudelEditor';
+import icon from './Assets/icon.png';
 
 function App() {
     return (
         <div className="App container-fluid">
-            <h2>Strudel Web App</h2>
+            <h2>
+                <img src={icon} alt="App Icon" style={{width:'60px', marginRight: '10px'}} />
+                Strudel Web App
+            </h2>
             <StrudelEditor />
         </div>
     );
