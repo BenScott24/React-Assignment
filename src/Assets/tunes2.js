fa2b561d6ea19a09b9033a5a78690925359a3ccd
BenScott24 <scobr003@mymail.unisa.edu.au>
2025-10-20 16:50:08 +1030

    adding an drum pattern

--- src/Assets/tunes2.js
+++ src/Assets/tunes2.js
@@ -12,7 +12,12 @@ x=>x.s("gm_kalimba:3").legato(1.5).attack(.025).release(.2).lp(1000).room(".6:2"
 x=>x.s("gm_acoustic_guitar_steel:6").clip(1.5).release(.2).room(".6:2").postgain(1)
 ).fast(2)
 
+let dr =
+stack( s("[bd:<1 0>(<3 1>,8,<0 2>:1.3)] , [~ sd:<15>:2.5]").note("B1").bank("LinnDrum")
+.decay(.3).room(".3:2").fast(2),
 
+s("[LinnDrum_hh(<3 2>,8)]").hp("1000").lp("9000").decay(.3).velocity([".8 .6"]).room(".3:2").fast(2),
+s("sh*8").note("B1").bank("RolandTR808").room(".6:2").velocity("[.8 .5]!4").postgain(1.5).fast(2))._pianoroll({vertical:0,flipTime:1,fill:0,labels:1})
 
 
 `
