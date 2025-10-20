77c7570b616ecf678f632e082d369b81a5eb82e4
BenScott24 <scobr003@mymail.unisa.edu.au>
2025-10-20 16:49:45 +1030

    making the second melody and adding an guitar to it

--- src/Assets/tunes2.js
+++ src/Assets/tunes2.js
@@ -4,4 +4,15 @@ let m1 =
 note("<[D@3 A@2 ~ D@2] [Cs@2 ~ A@2 ~ Cs@2]>".add("12,24")).s("gm_kalimba:3").legato(1.5).fast(2)
 .attack(.025).release(.2).lp(1000)
 .room(".6:2").postgain(1.5).color('#4dbcf4')._pitchwheel({edo:12,hapRadius:3,thickness:3,circle:1})
+
+let m2 = 
+note("<[D@3 A@2 ~ D@2] [Cs@2 ~ A@2 ~ Cs@2]>".add("12,24"))
+.layer(
+x=>x.s("gm_kalimba:3").legato(1.5).attack(.025).release(.2).lp(1000).room(".6:2").postgain(2),
+x=>x.s("gm_acoustic_guitar_steel:6").clip(1.5).release(.2).room(".6:2").postgain(1)
+).fast(2)
+
+
+
+
 `
