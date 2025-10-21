a62eb64be6d3a256a396f6f46eb8e09ff95f74fa
BenScott24 <scobr003@mymail.unisa.edu.au>
2025-10-21 12:09:59 +1030

    added useState to store editorInstance passed the globalEditor into controls

--- src/components/StrudelEditor.js
+++ src/components/StrudelEditor.js
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { StrudelMirror } from '@strudel/codemirror';
 import { evalScope } from '@strudel/core';
 import { drawPianoroll } from '@strudel/draw';
@@ -12,54 +12,56 @@ import Controls from './Controls';
 import CanvasRoll from './CanvasRoll';
 import { SetupButtons, Proc } from "./setupButtons";
 
-let globalEditor = null;
-
 export default function StrudelEditor() {
     const hasRun = useRef(false);
+    const [editorInstance, setEditorInstance] = useState(null);
 
     useEffect(() => {
     
-        if (!hasRun.current) {
-            console_monkey_patch();
-            hasRun.current = true;
+    if (hasRun.current) return;
+        hasRun.current = true;
 
-                const canvas = document.getElementById('roll');
-                canvas.width = canvas.width * 2;
-                canvas.height = canvas.height * 2;
-                const drawContext = canvas.getContext('2d');
-                const drawTime = [-2, 2]; 
+        console_monkey_patch();
+        initAudioOnFirstClick();
 
-                globalEditor = new StrudelMirror({
-                    defaultOutput: webaudioOutput,
-                    getTime: () => getAudioContext().currentTime,
-                    transpiler,
-                    root: document.getElementById('editor'),
-                    drawTime,
-                    onDraw: (haps, time) => drawPianoroll({ haps, time, ctx: drawContext, drawTime, fold: 0 }),
-                    prebake: async () => {
-                        initAudioOnFirstClick(); 
-                        const loadModules = evalScope(
-                            import('@strudel/core'),
-                            import('@strudel/draw'),
-                            import('@strudel/mini'),
-                            import('@strudel/tonal'),
-                            import('@strudel/webaudio'),
-                        );
-                        await Promise.all([loadModules, registerSynthSounds(), registerSoundfonts()]);
-                    },
-                });
-                
-            globalEditor.setCode(stranger_tune);
-            SetupButtons(globalEditor);
-            Proc(globalEditor);
-            }
-}, []);
+
+            const canvas = document.getElementById('roll');
+            canvas.width = canvas.width * 2;
+            canvas.height = canvas.height * 2;
+            const drawContext = canvas.getContext('2d');
+            const drawTime = [-2, 2]; 
+
+            const editor = new StrudelMirror({
+                defaultOutput: webaudioOutput,
+                getTime: () => getAudioContext().currentTime,
+                transpiler,
+                root: document.getElementById('editor'),
+                drawTime,
+                onDraw: (haps, time) => drawPianoroll({ haps, time, ctx: drawContext, drawTime, fold: 0 }),
+                prebake: async () => {
+                    const loadModules = evalScope(
+                        import('@strudel/core'),
+                        import('@strudel/draw'),
+                        import('@strudel/mini'),
+                        import('@strudel/tonal'),
+                        import('@strudel/webaudio'),
+                    );
+                    await Promise.all([loadModules, registerSynthSounds(), registerSoundfonts()]);
+                },
+            });
+            
+        editor.setCode(stranger_tune);
+        SetupButtons(editor);
+        Proc(editor);
+
+        setEditorInstance(editor);
+        }, []);
 
     return (
         <main className="editor-container">
             <div id="editor" />
             <CanvasRoll />
-            <Controls />
+            <Controls globalEditor={editorInstance} />
         </main>
     );
 }
