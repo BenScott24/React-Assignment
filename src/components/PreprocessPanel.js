3909f5eb68ea003aeead3a1bd87f6e754afb2ba5
BenScott24 <scobr003@mymail.unisa.edu.au>
2025-10-26 12:52:12 +1030

    adding bootstrap and card layout

--- src/components/PreprocessPanel.js
+++ src/components/PreprocessPanel.js
@@ -11,5 +11,20 @@ export default function PreprocessPanel({ onProcess, onProcessandPlay}) {
         if (onProcessandPlay) onProcessandPlay(text);
     };
 
-    
+    return (
+        <div className="container-fluid">
+            <div className="row">
+                <div className="col-md-8" style={{ maxHeigh:"50vh", overflowY:"auto"}} >
+                <label htmlFor="proc" className="form-label">Text to preprocess:</label>
+                <textarea className="form-control" rows="15" id="proc" value={text} onChange={(e) => setText(e.target.value)}></textarea>
+            </div>
+            <div className="col-md-4">
+                <nav>
+                    <button className="btn btn-outline-primary" onClick={handleProcess}>Preprocess</button>
+                    <button className="btn btn-outline-primary" onClick={handleProcessandPlay}>Proc and Play</button>
+                </nav>
+            </div>
+        </div>
+    </div>
+    );
 }
