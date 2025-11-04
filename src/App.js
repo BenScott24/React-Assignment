25d535ff2906594e56b3df7f500c53c3eccce01f
BenScott24 <scobr003@mymail.unisa.edu.au>
2025-11-04 16:32:12 +1030

    adding comments

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
+// Import global CSS for the app
 import './App.css';
+
+// Import the main Strudel editor component
 import StrudelEditor from './components/StrudelEditor';
+
+// Import logo image
 import logo from './Assets/logo.svg'
 
+// Main App component
 function App() {
     return (
+
+        // Root div for the entire application
         <div className="App container-fluid">
+
+            {/* Header section with logo and title */}
             <header className="app-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <h1 className="app-title">Strudel Web App</h1>
+
+                {/* Logo image */}
+                <img src={logo} className="App-logo" alt="logo" /> 
+
+                {/* Application title */}
+                <h1 className="app-title">Strudel Web App</h1> 
             </header>
+
+            {/* Main content section containing the Strudel editor */}
             <main className="app-main">
+
+                {/* Strudel editor component */}
                 <StrudelEditor />
             </main>
         </div>
     );
 }
 
+// Export the App component for rendering in index.js
 export default App;
